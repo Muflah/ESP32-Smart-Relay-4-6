ea580b76b82147dfcbd9a6ff8729a542b8f76095
Muflah <Muflah@users.noreply.github.com>
2020-10-23 20:13:26 +0500

    Update example.ino

--- SmartRelayControl/STM32F103C8/example/example.ino
+++ SmartRelayControl/STM32F103C8/example/example.ino
@@ -8,9 +8,7 @@ Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 void setup() {
   Serial.begin(9600);
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
-
   // Show image buffer on the display hardware.
   // Since the buffer is intialized with an Adafruit splashscreen
   // internally, this will display the splashscreen.
@@ -36,5 +34,18 @@ void setup() {
 
 void loop()
 {
+  //Serial.println("AT");
+  //Serial.println("AT + RESET");
+  if(Serial.available())
+  {
+    String xyz=Serial.readString();
+    display.setCursor(0, 0);
+    display.print(xyz);
+    display.display();
+    delay(500);
+    display.setCursor(0, 0);
+    display.clearDisplay();
+    display.display();
+  }
   delay(300);
 }
